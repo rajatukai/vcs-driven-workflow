10cc456af2b93de9bec6a6cb24e081683c2dea0a
rajatukai <82024354+rajatukai@users.noreply.github.com>
2023-12-07 17:13:30 +0530

    Update provider.tf
    removed some elements

--- provider.tf
+++ provider.tf
@@ -15,9 +15,6 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "333d9970-75d7-4ce4-86c2-26a818c62d9b"
-  tenant_id       = "df084ac0-4bbd-40ee-ac4a-d81e04cb8f11"
-  client_id       = "be9baeb4-e4ca-4d26-b9d2-4d276c425b05"
-  client_secret   = "6J88Q~ubCicSH_S3pvQhGHkrCnaqjHgyrQfGwaWi"
+
   features {}
 }
